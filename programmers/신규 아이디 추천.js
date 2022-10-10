e6398b1cd770aee91b6a4a7fa89ab3b6e667fedf
Minhye Kang <alsgp9898@naver.com>
2022-10-10 14:27:54 +0900

    정규표현식 관련 문제

--- programmers/신규 아이디 추천.js
+++ programmers/신규 아이디 추천.js
@@ -1,7 +1,7 @@
 function solution(new_id) {
   let answer, regexr;
   answer = new_id.toLowerCase();
-  regexr = /[^\d\a-z\-\_\.]/g;
+  regexr = /[^\d\a-z\-\_\.]/g; 
   answer = answer.replace(regexr, "");
   regexr = /\.{2,}/g;
   answer = answer.replace(regexr, ".");
@@ -16,6 +16,7 @@ function solution(new_id) {
     regexr = /\.$/g;
     answer = answer.replace(regexr, "");
   }
+  //repeat
   if (answer.length <= 2) {
     while (answer.length < 3) {
       answer += answer[answer.length - 1];
@@ -24,16 +25,17 @@ function solution(new_id) {
   return answer;
 }
 
-// 모법 풀이
+solution("...!@BaT#*..y.abcdefghijklm")
+/*
 function solution(new_id) {
-  const answer = new_id
-      .toLowerCase() // 1
-      .replace(/[^\w-_.]/g, '') // 2
-      .replace(/\.+/g, '.') // 3
-      .replace(/^\.|\.$/g, '') // 4
-      .replace(/^$/, 'a') // 5
-      .slice(0, 15).replace(/\.$/, ''); // 6
-  const len = answer.length;
-  return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+    const answer = new_id
+        .toLowerCase() // 1
+        .replace(/[^\w-_.]/g, '') // 2
+        .replace(/\.+/g, '.') // 3
+        .replace(/^\.|\.$/g, '') // 4
+        .replace(/^$/, 'a') // 5
+        .slice(0, 15).replace(/\.$/, ''); // 6
+    const len = answer.length;
+    return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
 }
-
+*/
