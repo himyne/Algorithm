38443809541e0adeafeb8f1fa7aad0acc158fe77
Minhye Kang <88049162+himyne@users.noreply.github.com>
2023-04-14 21:26:03 +0900

    [level 2] Title: 모음 사전, Time: 0.78 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,30 +1,59 @@
+// function solution(word) {
+//     const alph = ['A', 'E', 'I', 'O', 'U'];
+    
+//     let count = 0;
+//     let answer;
+//     let result = false;
+    
+//     const dfs = (str) => {
+        
+//         if(str === word) {
+//             answer = count;
+//             result = true;
+//             return;
+//         }
+        
+//         if(str.length === 5 || result) return;
+//         for(let i=0; i<alph.length; i++){
+//             const nextWord = str + alph[i];
+//             count++;
+//             dfs(nextWord);
+//         }
+//     }
+//     dfs('');    
+    
+//     return answer;
+// }
 function solution(word) {
-    const alph = ['A', 'E', 'I', 'O', 'U'];
-    
-    let count = 0;
-    let answer;
-    let result = false;
-    
-    const dfs = (str) => {
-        
-        if(str === word) {
-            answer = count;
-            result = true;
-            return;
-        }
-        
-        if(str.length === 5 || result) return;
-        for(let i=0; i<alph.length; i++){
-            const nextWord = str + alph[i];
-            count++;
-            dfs(nextWord);
+    const words = gererateAllWords();
+    return words.indexOf(word) + 1;
+}
+
+
+function gererateAllWords() {
+    const letters = ['A', 'E', 'I', 'O', 'U'];
+    const LEN = letters.length;
+    const words = [];
+    for(let i = 0; i < LEN; i++) {
+        words.push(letters[i]);
+        for(let j = 0; j < LEN; j++) {
+            words.push(letters[i] + letters[j]);
+            for(let k = 0; k < LEN; k++) {
+                words.push(letters[i] + letters[j] + letters[k]);
+                for(let l = 0; l < LEN; l++) {
+                    words.push(letters[i] + letters[j] + letters[k] + letters[l]);
+                    for(let m = 0; m < LEN; m++) {
+                        words.push(letters[i] + letters[j] + letters[k] + letters[l] + letters[m]);
+                    }
+                }
+            }
         }
     }
-    dfs('');    
-    
-    return answer;
+    return words;
 }
 /*
+
+
 A
 AA
 AAA
