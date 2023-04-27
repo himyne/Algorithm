ef70f59e52a68ff4f89300e3798cdf5544040ea6
Minhye Kang <88049162+himyne@users.noreply.github.com>
2023-04-27 10:27:57 +0900

    [level 3] Title: 기지국 설치, Time: 8.25 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.js
@@ -4,25 +4,30 @@ function solution(n, stations, w) {
     const result = [];
     let count = 0;
     
+    // 기지국이 전파시키는 아파트 위치 저장
     for(let station of stations){
         let start = (station-w) < 1 ? 1 : station-w;
         let end = (station+w) > n ? n : station+w;
         connections.push([start, end])
     }
     
+    // 기지국 사이사이에 도달하지 못한 아파트들
     for(let i=0; i<connections.length-1; i++){
         result.push(connections[i+1][0]-connections[i][1]-1)
     }
     
+    // 앞쪽에 도달하지 못한 아파트들
     if(connections[0][0] !== 1) {
         result.push(connections[0][0]-1);
     }
     
+    // 뒤쪽에 도달하지 못한 아파트들
     if(connections[connections.length-1][1] !== n){
         result.push(n-connections[connections.length-1][1])
     }
     
-    const amount = w*2+1;
+    const amount = w*2+1; 
     
+    // 전파량으로 나눈 몫을 올림한 값들을 모두 더한다.
     return result.reduce((acc, cur) => acc += Math.ceil(cur/amount), 0);
 }
