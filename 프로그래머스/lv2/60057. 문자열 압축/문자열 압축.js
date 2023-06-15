32f79306b53b732b347f80f54346fbfb32ca6ec0
Minhye Kang <88049162+himyne@users.noreply.github.com>
2023-06-15 13:37:21 +0900

    [level 2] Title: 문자열 압축, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -1,31 +1,31 @@
 function solution(s) {
+    // abc abc ded e
+    const a = [];
     if(s.length === 1) return 1;
-    const lenArr = [];
-    
     for(let i=1; i<s.length; i++) {
         const answer = [];
         const result = []; 
-        
         const str = [...s];
         for(let j=0; j<s.length/i; j++){
             const slicedArr = str.splice(0, i);
             result.push(slicedArr)
         }
-        
+
         let count = 1;
         for(let k=0; k<result.length-1; k++){
-            const a = result[k].join('');
-            const b = result[k+1].join('');
-            
-            if(a === b) count++;
-            
-            if(a !== b){
-                answer.push(count===1 ? '' : count, a);
-                count = 1;
+            if(result[k].join('') === result[k+1].join('')){
+                count = count + 1;
             }
-            if(k === result.length-2) answer.push(count===1 ? '' : count, b);
+            if(result[k].join('') !== result[k+1].join('')){
+                answer.push(count===1 ? '' : count, result[k].join(''));
+                count = 1
+            }
+            if(k === result.length-2){
+                answer.push(count===1 ? '' : count, result[k+1].join(''))
+            }
         }
-        lenArr.push(answer.join('').length);
+        a.push(answer.join('').length);
     }
-    return Math.min(...lenArr)
+    return Math.min(...a)
 }
+
